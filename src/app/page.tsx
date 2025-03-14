111543da02648d80c2370d8a38cab79cdcc350b9
Sabter Iqbal <sabterrazaqadri@gmail.com>
2025-03-14 14:48:47 +0500

    Update page.tsxad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import ZakatCalculator from './components/ZakatCalculator'
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
+      async function add() {
+        <script type='text/javascript' src='//pl26109967.effectiveratecpm.com/45/54/af/4554afae580009c06b3ac05b7c7033d1.js'></script>
+      }
       <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 dark:text-white">
         Zakat Calculator
       </h1>
